5d3a28970efca9afb262369b9f3541618876b6e4
cyjseagull <yujiechen_hust@163.com>
2021-06-11 14:36:20 +0800

    fix InstallInclude

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework
-VERSION 3.0.0-local-1b0c44fb
-URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/1b0c44fbffe7b02ff9aa5c689946cb21d73d7e9d.tar.gz"
-SHA1 23eb4e9153ba85b25ec1dfb93ad89eec56d41e18
+VERSION 3.0.0-local
+URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/62642fa202886e1470d78ad521002b36a2e86160.tar.gz"
+SHA1 8eb63a8919041e8d2c432dcbc626d6c34934b5ae
 )
