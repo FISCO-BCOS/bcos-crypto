f3e65de5e06cec5c9f468116b530fd070f1c367b
cyjseagull <yujiechen_hust@163.com>
2021-12-06 22:39:06 +0800

    update version to 3.0.0-rc1

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,11 +1,5 @@
-hunter_config(bcos-framework
-VERSION 3.0.0-local
-URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/946fb10bc67119c9646c22d515cafb2e1390e5f9.tar.gz"
-SHA1 550b02ccc4e35497e5eaa8cbf5ac66a2d53d6031
-)
-
-hunter_config(wedpr-crypto VERSION 1.1.0-10f314de
-	URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/10f314de45ec31ce9e330922b522ce173662ed33.tar.gz
-	SHA1 626df59f87ea2c6bb5128f7d104588179809910b
-	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON
+hunter_config(bcos-framework VERSION 3.0.0-local
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/80345086f18586ef1e6122d7bf5eb9688e990472.tar.gz
+	SHA1 1fabfc8bb45d3793f5a4789a01f32fb35ec691e2
+	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
