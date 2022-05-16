1c0752a4fec97486738293f242624fe856d16908
morebtcg <651932351@qq.com>
2022-05-16 19:28:27 +0800

    Remove dep

--- bcos-cmake-scripts/InstallBcosUtilities.cmake
+++ bcos-cmake-scripts/InstallBcosUtilities.cmake
@@ -1,4 +1,6 @@
-hunter_add_package(Boost COMPONENTS all)
+if(HUNTER_BOOST)
+    hunter_add_package(Boost COMPONENTS all)
+endif()
 hunter_add_package(bcos-utilities)
 find_package(Boost CONFIG REQUIRED log chrono system filesystem iostreams thread program_options)
 find_package(bcos-utilities CONFIG REQUIRED)
