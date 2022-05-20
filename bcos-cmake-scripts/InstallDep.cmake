d06e8cd82ad15616b7b38056e202ad9ed881c354
morebtcg <651932351@qq.com>
2022-05-20 15:40:58 +0800

    Reduce wedpr

--- bcos-cmake-scripts/InstallDep.cmake
+++ bcos-cmake-scripts/InstallDep.cmake
@@ -36,7 +36,7 @@ else()
             URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/0e3ca8614808825da4f91acc51e1031a5184119e.tar.gz
             URL_HASH SHA1=dcbb69c96085ada1d107380b3771fd8e177ad207
             CONFIGURE_COMMAND ""
-            BUILD_COMMAND cargo +nightly-2021-06-17 build --release --manifest-path ${CMAKE_CURRENT_BINARY_DIR}/wedpr-crypto-prefix/src/wedpr-crypto/Cargo.toml
+            BUILD_COMMAND cargo +nightly-2021-06-17 build --release --features "wedpr_f_hash_keccak256 wedpr_f_signature_secp256k1 wedpr_f_signature_sm2 wedpr_f_hash_sm3 wedpr_f_vrf_curve25519 wedpr_f_crypto_block_cipher_aes wedpr_f_crypto_block_cipher_sm4 wedpr_f_hash_ripemd160 wedpr_f_hash_sha2 wedpr_f_hash_sha3 wedpr_f_hash_blake2b wedpr_f_signature_ed25519" --no-default-features --manifest-path ${CMAKE_CURRENT_BINARY_DIR}/wedpr-crypto-prefix/src/wedpr-crypto/ffi/ffi_c/ffi_c_crypto_binary/Cargo.toml
             INSTALL_COMMAND cp -r ${CMAKE_CURRENT_BINARY_DIR}/wedpr-crypto-prefix/src/wedpr-crypto/third_party/include ${WEDPR_CRYPTO_INSTALL}/include/wedpr-crypto
         )
 
